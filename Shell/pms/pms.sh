faae1fed38e0927879d78a80f70a8baaa01496f5
Brenton Horne <brentonhorne77@gmail.com>
2018-10-25 18:24:53 +1000

    Adding extra aliases for PMS

--- Shell/pms/pms.sh
+++ Shell/pms/pms.sh
@@ -8,9 +8,14 @@ function spiy {
   sudo eopkg install -y "$@"
 }
 
+alias spi=spiy
+alias sei=spiy
+
 function spry {
   sudo eopkg remove -y "$@"
 }
+
+alias spr=spry
 
 function sps {
   sudo eopkg search "$@"
